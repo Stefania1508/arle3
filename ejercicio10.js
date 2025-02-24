7346137eceba09a715e88c6066ba135c46ad3c8f
stefania <stefaniacobalondon@gmail.com>
2025-02-23 21:40:18 -0500

    c

--- ejercicio10.js
+++ ejercicio10.js
@@ -1,32 +1,3 @@
-// const readline = require("readline");
-
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout
-// });
-
-// rl.question("¿Cuántas copias deseas imprimir? ", (cantidad) => {
-//   cantidad = Number(cantidad);
-//   let precioPorCopia = 0;
-
-//   if (cantidad >= 1000) {
-//     precioPorCopia = 50;
-//   } else if (cantidad >= 750) {
-//     precioPorCopia = 80;
-//   } else if (cantidad >= 500) {
-//     precioPorCopia = 100;
-//   } else {
-//     precioPorCopia = 120;
-//   }
-
-//   let precioTotal = cantidad * precioPorCopia;
-
-//   console.log(`El precio por copia es: $${precioPorCopia}`);
-//   console.log(`El precio total a pagar es: $${precioTotal}`);
-
-//   rl.close();
-// });
-
 let copias = 0;
 while (true){
     copias = Number(prompt(`Ingrese la cantidad que desea imprimir`));
